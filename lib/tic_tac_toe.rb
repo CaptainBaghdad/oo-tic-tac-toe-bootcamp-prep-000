9744b60021cf57add64290a435711d383a66756d
Jason Pitts <juniorjr083@gmail.com>
2018-05-23 02:06:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,7 +117,16 @@ end
 end
   
   
-  
+  def play()
+  while over?() == false
+    turn()
+  end
+  if won?()
+    puts "Congratulations #{winner()}!"
+  elsif draw?()
+    puts "Cat\'s Game!"
+  end
+end
   
   
   
