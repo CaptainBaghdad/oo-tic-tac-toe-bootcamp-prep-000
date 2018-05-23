d5f915d36f78f4f9153e0d65b4ac6e34c27a20a9
Jason Pitts <juniorjr083@gmail.com>
2018-05-23 01:54:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,20 @@ class TicTacToe
   
   
   
+  def turn() 
+ puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player(@board)
+    if valid_move?(@board, index)
+      move(@board, index, char)
+      display_board(@board)
+    else
+      turn(@board)
+    end
   
+  
+end  
   
   
   
